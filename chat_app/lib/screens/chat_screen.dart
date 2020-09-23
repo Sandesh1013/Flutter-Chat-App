71c0fc1ac59c8a610de9b8f0759aa880b1ecfadb
Sandesh Singh <sandesh1014@gmail.com>
2020-09-23 18:10:27 +0530

    Fixed bug in Firestore SDK

--- chat_app/lib/screens/chat_screen.dart
+++ chat_app/lib/screens/chat_screen.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-      title:Text('Chat Screen'),
-    ),
-      body: ListView.builder(itemCount:10,itemBuilder: (ctx,index)=>Container(
-        padding: EdgeInsets.all(8),
-        child: Text('Ofc this works'),
+      appBar: AppBar(
+        title: Text('Chat Screen'),
       ),
-    ));
+      body: ListView.builder(itemCount: 10, itemBuilder: (ctx, index) =>
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Text('Ofc this works'),
+          ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance.collection('chats/eM5tagy4XmMMStELY2bG/messages')
+              .snapshots().listen((data){
+                print(data);
+          });
+        },
+      ),);
   }
 }
