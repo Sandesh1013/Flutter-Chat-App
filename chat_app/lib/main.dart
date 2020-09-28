310c6ce200f8541f13f5172440dbe4d76598da52
Sandesh Singh <sandesh1014@gmail.com>
2020-09-28 21:19:40 +0530

    Fixed Bug in Files

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
-import 'screens/chat_screen.dart';
-import 'screens/auth_screen.dart';
-void main() {
-  runApp(MyApp());
-}
+import 'package:firebase_auth/firebase_auth.dart';
+import'./screens/auth_screen.dart';
+import'./screens/chat_screen.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chat App',
+      title: 'FlutterChat',
       theme: ThemeData(
         primarySwatch: Colors.pink,
         backgroundColor: Colors.pink,
         accentColor: Colors.deepPurple,
         accentColorBrightness: Brightness.dark,
         buttonTheme: ButtonTheme.of(context).copyWith(
-          buttonColor:Colors.pink,
+          buttonColor: Colors.pink,
           textTheme: ButtonTextTheme.primary,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
         ),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: AuthScreen(),
+      home: StreamBuilder(stream: FirebaseAuth.instance.onAuthStateChanged, builder: (ctx, userSnapshot) {
+        if (userSnapshot.hasData) {
+          return ChatScreen();
+        }
+        return AuthScreen();
+      }),
     );
   }
 }
